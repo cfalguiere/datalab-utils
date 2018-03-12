cd2479f207b1a478bf8918e1cdb497dd4e79424d
Claude Falguiere <cfalguiere@gmail.com>
2018-03-12 01:26:51 +0100

    cleanup

--- customization/schapp.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf-8 -*-
-from __future__ import print_function
-
-from sch.common.app import AppContext
-
-class SCHAppContext(AppContext):
-
-    def __init__(self):
-          AppContext.__init__('SCH')
